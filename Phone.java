1a0b47cf80f96835d9a6ddcbcc657939ac73e86b
ALEH BIAHUN <stones2875@gmail.com>
2023-05-13 08:04:51 +0200

    Выполнение пункта 5, 6, 7

--- Phone.java
+++ Phone.java
@@ -3,10 +3,25 @@ public class Phone {
     String model;
     int weight;
 
+    Phone(int num, String mod, int wei){
+        number = num;
+        model = mod;
+        weight = wei;
+    }
+
+    Phone(int num, String mod){
+        number = num;
+        model = mod;
+    }
+
+    Phone(){
+
+    }
+
     void receiveCall(String name) {              //создали метод receiveCall
         System.out.println("Звонит " + name);
-    }
+    } //добавили в класс Phone метод receiveCall
     int getNumber() {                           //создали метод getNumber (возвращает телефон)
         return number;
-    }
+    }   //добавили в класс Phone метод getNumber, который возвращает number
 }
